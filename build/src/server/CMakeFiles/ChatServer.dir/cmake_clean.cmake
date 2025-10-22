d196d6af36d94290606db7fa89055009c6d0e42e
krantz0309 <2290338457@qq.com>
2025-10-22 17:20:58 +0800

    changeofCMakeLists

---
+++ build/src/server/CMakeFiles/ChatServer.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "/home/krantz/Projects/ClusterChatServer/ClusterChatServer/bin/ChatServer"
+  "/home/krantz/Projects/ClusterChatServer/ClusterChatServer/bin/ChatServer.pdb"
+  "CMakeFiles/ChatServer.dir/chatserver.cpp.o"
+  "CMakeFiles/ChatServer.dir/chatserver.cpp.o.d"
+  "CMakeFiles/ChatServer.dir/main.cpp.o"
+  "CMakeFiles/ChatServer.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ChatServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
